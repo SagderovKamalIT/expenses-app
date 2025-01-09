3d75f66eb3390d6e2ef1dbea103116c25c0d4759
Kamal Sagderov <sagderovkamal.it@gmail.com>
2025-01-09 14:28:31 +0300

    add limit function to the app

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const expenses = [];
 
+const LIMIT = 10000;
+
 // Node позволяет понять, что это часть html-кода
 const inputNode = document.querySelector('.js-expense-input');
 const buttonNode = document.querySelector('.js-expense-button');
 const historyNode = document.querySelector('.js-history');
+const sumNode = document.querySelector('.js-sum');
+const limitNode = document.querySelector('.js-limit');
+const statusNode = document.querySelector('.js-status');
+
+limitNode.innerText = LIMIT;
 
 buttonNode.addEventListener('click', function() {
     // 1. Получаем значение из поля ввода
@@ -26,10 +33,28 @@ buttonNode.addEventListener('click', function() {
     let expensesListHTML = '';
 
     expenses.forEach(element => {
-        expensesListHTML += `<li>${element}</li>`;
+        expensesListHTML += `<li>${element} руб.</li>`;
     });
 
    
     historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+
+    // 4.считаем сумму и выводи ее
+    let sum = 0;
+    expenses.forEach(element => {
+        sum += element;
+    });
+
+    console.log(sum);
+
+    sumNode.innerText = sum;
+
+    // 5. Сравнение с лимитом и вывод статуса
+    if (sum <= LIMIT) {
+        statusNode.innerText = 'все хорошо';
+    } else {
+        statusNode.innerText = 'все плохо';
+        statusNode.classList.add('status-red');
+    }
 });
 
